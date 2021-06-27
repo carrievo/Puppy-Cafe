3b31eaede7f016405227a95a3b449bf183abfcb8
Carrie Vo <carrienhivo@gmail.com>
2021-06-26 17:49:29 -0700

    added JS prompts and made changes to html

--- script.js
+++ script.js
@@ -6,29 +6,29 @@ let answer = prompt('what is your name?'); //carrie
 
 // we are getting the date and hour of the current time
 let time = new Date().getHours();
+let timeofday = '';
 
+
+if(time < 12){
+    timeofday = 'Good morning, ';
+} else if (time <17){
+    timeofday = 'Good afternoon, ';
+} else{
+    timeofday = 'Good evening, ';
+}
 if(time > 12){
     time = time - 12 + 'pm';
 } else {
     time = time + 'am';
 }
 
-if (time < 12){
-    timeofday = 'good morning';
-} else if (time <17){
-    timeofday = 'good afternoon';
-} else{
-    timeofday = 'good evening';
-}
-
-
-// this variable exists, but i;m not assigning a value yet
+// this variable exists, but i'm not assigning a value yet
 let greeting = '';
 
 if(answer === 'carrie'){
     greeting = timeofday + ' ' + answer + '. I hope you have a great day. The hour time is ' + time;
 } else{
-    greeting = timeofday + 'you cannot pass go, you cannot collect $200. The hour time is ' + time;
+    greeting = timeofday + 'today is a good day. The time right now is ' + time;
 }
 
 
