218dd7bf691cf7e689a01c845581089afdbc7fda
Carrie Vo <carrienhivo@gmail.com>
2021-06-29 14:40:37 -0700

    added functions to script.js

--- script.js
+++ script.js
@@ -1,35 +1,58 @@
 'use strict'
 
+function getName(){
 //make some logic that will populate that bit of text on my webpage
-
-let answer = prompt('what is your name?'); //carrie
+let name = prompt('what is your name?'); //carrie
+    return name;
+}
 
 // we are getting the date and hour of the current time
 let time = new Date().getHours();
-let timeofday = '';
 
-
-if(time < 12){
-    timeofday = 'Good morning, ';
-} else if (time <17){
-    timeofday = 'Good afternoon, ';
-} else{
-    timeofday = 'Good evening, ';
+function getTimeOfDay(time){
+    let timeofDay = '';
+    if(time < 12){
+        timeofDay = 'Good morning, ';
+    } else if (time <17){
+        timeofDay = 'Good afternoon, ';
+    } else{
+        timeofDay = 'Good evening, ';
 }
-if(time > 12){
-    time = time - 12 + 'pm';
-} else {
-    time = time + 'am';
+    return timeofDay;
 }
 
-// this variable exists, but i'm not assigning a value yet
-let greeting = '';
+function getamOrpm(time){
 
-if(answer === 'carrie'){
-    greeting = timeofday + ' ' + answer + '. I hope you have a great day. The hour time is ' + time;
-} else{
-    greeting = timeofday + 'today is a good day. The time right now is ' + time;
+    if(time > 12){
+       time = time - 12 + 'pm';
+    } else {
+        time = time + 'am';
+    }
+    return time;
 }
 
+// we are getting the date and hour of the current time
 
+function getGreeting(timeofday, name, ampm){
+    let greeting = '';
+
+    if(name === 'Carrie'){
+        greeting = timeofday + ' ' + name + '. WOOF WOOF! I hope you have a great day. The hour time is ' + ampm;
+    } else{
+        greeting = timeofday + ' WOOF WOOF! Today is a good day. The time right now is ' + ampm;
+    }
+
+    return greeting;
+
+}
+
+function writeToDocument(greeting){
 document.write(greeting);
+}
+
+// call all of our required functions
+let timeOfDay = getTimeOfDay(time);
+let name = getName();
+let ampm = getamOrpm(time);
+let greeting = getGreeting(timeOfDay, name, ampm);
+writeToDocument(greeting);
